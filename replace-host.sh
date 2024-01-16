322b9be34424c47abf9ee9de8cd59e6586d70023
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2024-01-16 18:05:01 +0100

    explain construction of the VM hostname

--- replace-host.sh
+++ replace-host.sh
@@ -5,5 +5,6 @@ DOMAIN=${1:-$DOMAIN}
 
 echo "Use domain '$DOMAIN'"
 for ingressFile in *ingress*.yaml; do
+  # shellcheck disable=SC2086
   sed -i 's/\DOMAIN_PLACEHOLDER/'${DOMAIN}'/g' "$ingressFile"
 done
